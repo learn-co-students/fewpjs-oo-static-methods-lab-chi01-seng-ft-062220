807b7066f1183ac3c43f889dce4295a2d1020b7d
foxten <tenaysiafox@gmail.com>
2020-08-18 06:53:04 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '');
+  }
+
+  static titleize(string){
+    let skipCapital = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by','from']
+    let data = string.split(" ")
+    let completeTitle = []
+    data.forEach(word => {
+      if(skipCapital.includes(word) && word != data[0]){
+        completeTitle.push(word)
+      }else{
+       completeTitle.push(this.capitalize(word))
+      }
+    })
+    return completeTitle.join(' ')
+  }
+
 }
